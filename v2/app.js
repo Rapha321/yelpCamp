af2bb8b745c708e806a58ce00445d0a5545d8f74
Rapha321 <ralpha19@hotmail.com>
2019-02-22 23:08:27 +0000

    added mongoose

--- v2/app.js
+++ v2/app.js
@@ -3,7 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose")
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -13,21 +13,22 @@ var campgroundSchema = new mongoose.Schema({
   image: String
 });
 
-var Campground = mongoose.model("campground", campgroundSchema);
+var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-  {
-      name: "Salmon Creek",
-      image: "https://www.appletonmn.com/vertical/Sites/%7B4405B7C1-A469-4999-9BC5-EC3962355392%7D/uploads/campground_(2).jpg"
+// Campground.create(
+//   {
+//       name: "Granite Hill",
+//       image: "https://api.campgroundbooking.com/api/v1/property/5a3a0399d9c7ef48b9a3be5f/image/Tent%20platform%20at%20nelson%20city%20campground_600_thumb.jpg"
+//   },
 
-  }, function(err, campground) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log("NEWLY CREATED CAMPGROUND: ");
-      console.log(campground);
-    }
-  });
+//   function(err, campground) {
+//     if(err) {
+//       console.log(err);
+//     } else {
+//       console.log("NEWLY CREATED CAMPGROUND: ");
+//       console.log(campground);
+//     }
+//   });
 
 
 
@@ -49,7 +50,14 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res){
-  res.render("campgrounds", {campgrounds: campgrounds});
+  // Get all campground from DB
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", {campgrounds: allCampgrounds});
+    }
+  });
 });
 
 app.post("/campgrounds", function(req, res) {
@@ -57,10 +65,15 @@ app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = {name: name, image: image}
-  campgrounds.push(newCampground);
-
-  // redirect back to campgrounds page
-  res.redirect("/campgrounds")
+  // Create a new campground and save to DB
+  Campground.create(newCampground, function(err, newlyCreated){
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to campgrounds page
+      res.redirect("/campgrounds");
+    }
+  })
 });
 
 app.get("/campgrounds/new", function(req, res) {
